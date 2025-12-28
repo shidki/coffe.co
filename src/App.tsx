b7c218cae533e1af43fa6c26e59ff8eba6aaee06
shidki <shidkigaming7@gmail.com>
2025-12-28 19:10:13 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const App: React.FC = () => {
       let basePage = 5;
       switch(currentPage) {
         case 'home':
-          basePage = isSmallMobile ? 8.5 : isMobile ? 8.5 : isTablet ? 5 : 6;
+          basePage = isSmallMobile ? 7.8 : isMobile ? 7.8 : isTablet ? 5 : 6;
           break;
         case 'about':
           basePage = isSmallMobile ? 13 : isMobile ? 13 : isTablet ? 5.7 : 8;
